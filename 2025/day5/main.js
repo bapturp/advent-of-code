66ab9b9b56686c69692f38089d1536a74a768afd
Baptiste Dromer <baptiste.dromer@gmail.com>
2025-12-09 22:36:17 +0100

    day5 part1

--- 2025/day5/main.js
+++ 2025/day5/main.js
@@ -4,6 +4,30 @@ async function loadData(fileName) {
   const url = new URL(fileName, import.meta.url)
   const contents = await readFile(url, 'utf-8')
   return contents
+    .split('\n\n')
+    .map((e) => e.trim().split('\n'))
+    .map((e) =>
+      e.map((f) =>
+        f.includes('-') ? f.split('-').map((g) => Number(g)) : Number(f)
+      )
+    )
 }
 
-console.log(await loadData('./example.txt'))
+function numberOfFreshIngredients(contents) {
+  let fresh = []
+
+  const [ranges, available] = contents
+
+  for (const ingredient of available) {
+    ranges.forEach((range) => {
+      if (ingredient >= range[0] && ingredient <= range[1]) {
+        console.log(`${ingredient} is fresh`)
+        fresh.push(ingredient)
+      }
+    })
+  }
+
+  return Array.from(new Set(fresh)).length
+}
+
+console.log(numberOfFreshIngredients(await loadData('./input.txt')))
